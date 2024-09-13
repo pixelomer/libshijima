9ed92370ef2d7d68171b4dfa7f8691bc727f7aeb
pixelomer <37103802+pixelomer@users.noreply.github.com>
2024-09-13 12:59:20 +0300

    Fix build with GCC

--- shijima/action/jump.hpp
+++ shijima/action/jump.hpp
@@ -15,7 +15,7 @@ public:
         distance.x = target.x - mascot->anchor.x,
         distance.y = target.y - mascot->anchor.y - std::abs(distance.x);
         double velocity_abs = vars.get_num("VelocityParam", 20);
-        double distance_abs = std::sqrtf(distance.x*distance.x + distance.y*distance.y);
+        double distance_abs = sqrtf(distance.x*distance.x + distance.y*distance.y);
         if (distance_abs != 0) {
             math::vec2 velocity { velocity_abs * distance.x / distance_abs,
                 velocity_abs * distance.y / distance_abs };
