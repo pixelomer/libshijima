48f303fd8e1a9facaf44176f41e156546414aada
pixelomer <37103802+pixelomer@users.noreply.github.com>
2025-01-17 10:46:30 +0300

    Fix LookRight default value for Look action

--- shijima/action/look.cc
+++ shijima/action/look.cc
@@ -4,7 +4,7 @@ namespace shijima {
 namespace action {
 
 bool look::tick() {
-    mascot->looking_right = vars.get_bool("LookRight", false);
+    mascot->looking_right = vars.get_bool("LookRight", !mascot->looking_right);
     return false;
 }
 
