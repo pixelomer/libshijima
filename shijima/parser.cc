pixelomer/libshijima

commit cda8dce6c68bb3245098cc1b97f2f182b9ff34d1

fix: skip unexpected behavior references

shijima/parser.cc
@@ -517,11 +517,13 @@ behavior::list parser::parse_behavior_list(pugi::xml_node root,
         std::string name = node.name();
         if (name == "Behavior" || name == "BehaviorReference") {
             bool reference = (name == "BehaviorReference");
-            if (reference && !allow_references) {
-                fail("BehaviorReference in unexpected location");
-            }
             auto attr = all_attributes(node, {{ "Name", "" }, { "Condition", "true" },
                 { "Hidden", "false" }, { "Frequency", "0" }});
+            if (reference && !allow_references) {
+                warn("BehaviorReference in unexpected location (name="
+                    + attr.at("Name") + ", frequency=" + attr.at("Frequency") + ")");
+                continue;
+            }
             int freq = std::stoi(attr.at("Frequency"));
             bool hidden = (attr.at("Hidden") == "true");
             auto behavior = std::make_shared<behavior::base>(attr.at("Name"),

commit 8b909017be43bb695143aed9b32f3198afc9b1d6

fix: infinite loop while parsing behaviors
regression in cda8dce6c68bb3245098cc1b97f2f182b9ff34d1

shijima/parser.cc
@@ -522,6 +522,7 @@ behavior::list parser::parse_behavior_list(pugi::xml_node root,
             if (reference && !allow_references) {
                 warn("BehaviorReference in unexpected location (name="
                     + attr.at("Name") + ", frequency=" + attr.at("Frequency") + ")");
+                node = node.next_sibling();
                 continue;
             }
             int freq = std::stoi(attr.at("Frequency"));